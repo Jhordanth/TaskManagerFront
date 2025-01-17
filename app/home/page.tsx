500d075d4780896a1b8868280e1b7b078867ad40
Jhordanth <jhordanthfabian@gmail.com>
2025-01-16 19:25:53 -0500

    ProjectStructure

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/button';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Button text="button" />
+    </div>
+  );
+};
+export default Home;
